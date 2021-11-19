b8e324b1c7a72bad95af32d4f39784d352270e66
Zajo <ckkoval33@gmail.com>
2021-11-19 12:02:57 +0100

    kosik fully made

--- E-shop/app/Http/Controllers/CartController.php
+++ E-shop/app/Http/Controllers/CartController.php
@@ -8,61 +8,172 @@ use App\Models\Product;
 use App\Models\User_cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Session;
+use PHPUnit\TextUI\XmlConfiguration\Php;
 
 class CartController extends Controller
 {
     
     public function add_to_cart(Request $request){
 
-        $user = Auth::user();
-        $id = Auth::id();
-        $product_id = request('id');
-        $count = request('quantity');
-        echo($product_id);
         
-        $cart = User_cart::firstOrCreate([
-            'user_id' => $id,
-        ]);
-        $new_id = $cart->id;
-        
-        
-        
-        $cartitem = new Cart_items;
-        $cartitem->product = $product_id;
-        $cartitem->counter = $count;
-        $cartitem->cart_id = $new_id;
 
-        $cartitem->save();
-        return redirect('/kosik');
+        if (Auth::check()) {
+            $user = Auth::user();
+        
+            $id = Auth::id();
+            $product_id = request('id');
+            $count = request('quantity');
+            
+            
+            $cart = User_cart::firstOrCreate([
+                'user_id' => $id,
+            ]);
+            $new_id = $cart->id;
+
+            $exact_product = Cart_items::where('product', '=', $product_id);
+            $exact_product = $exact_product->first();
+            
+            if ($exact_product == null) {
+                $cartitem = new Cart_items;
+                $cartitem->product = $product_id;
+                $cartitem->counter = $count;
+                $cartitem->cart_id = $new_id;
+
+                $cartitem->save();
+            }
+            else{
+                $exact_product->counter = $exact_product->counter +$count;
+               
+                $exact_product->save();
+            }
+            
+            return redirect('/kosik');
+        }
+
+        else
+        {
+       
+            if (Session::exists('cart')) 
+            {
+                $variable = 1;
+                $id = request('id');
+                $counter = request('quantity');
+                $kosik = Session::get('cart');
+                $newkosik=[];
+                foreach ($kosik as $item){
+                    if ($item[0] === $id){
+                        $item[1] = $item[1] + $counter;
+                        $variable = 0;
+                        
+                    }
+                    array_push($newkosik,$item);
+                }
+                if ($variable == 1)
+                {
+                    $new_item = [$id,$counter];
+
+                    array_push($newkosik,$new_item);
+                }
+                
+
+                Session::put('cart',$newkosik);
+                Session::save();
+                return redirect('/kosik');
+            }
+
+            else
+            {   
+                $cart =[];
+                Session::put('cart', $cart);
+                Session::save();
+
+                $id = request('id');
+                $counter = request('quantity');
+   
+                $cartitem = [$id,$counter];
+                
+                Session::push('cart',$cartitem);
+                Session::save();
+              
+                return redirect('/kosik');
+                
+            }
+        }
+        
+        
+        
         
     }
 
 
-    public function kosik()
+    public function kosik(Request $request)
     {
+
         $id = Auth::id();
         
         $cart = User_cart::all()->where('id','=',$id);
-        
-        $cart = $cart->first();
 
+        if (count($cart) == 0)
+            {
+                $list = [];
+            }
+
+        if (Auth::check()) 
+        {
+            $cart = $cart->first();
+            $items = Cart_items::all()->where('cart_id','=',$cart->id);
         
-        $items = Cart_items::all()->where('cart_id','=',$cart->id);
+            $list = [];
+            foreach ($items as $item){
+                $number = $item->product;
+                $counter = $item->counter;
+                $item = Product::all()->where('id','=',$number);
+                $item = $item->first();
+                $produkt = [$item,$counter];
+                array_push($list,$produkt);
+                
+            }
+            return view('kosik')->with('items',$list)
+            ->with('cart_id',$cart->id);
+        }
         
 
-        
-        
-        $list = [];
-        foreach ($items as $item){
-            $number = $item->product;
-            $produkt = Product::all()->where('id','=',$number);
+        else
+        {
+
+            $value = Session::get('cart');
             
-            array_push($list,$produkt->first());
+            if (Session::has('cart'))
+            {
+                foreach($value as $item)
+                {
+                    $product = $item[0];
+                    $counter = $item[1];
+                    $item = Product::where('id',$product);
+                    $item = $item->first();
+                    $produkt = [$item,$counter];
+                    
+                    array_push($list,$produkt);
+                }
+            }
+            else
+            {
+                $list = [];
+            }
+            $cart_id = 0;
+            return view('kosik')->with('items',$list)
+            ->with('cart_id',$cart_id);
+            
+  
         }
         
         
-        return view('kosik')->with('items',$list);
+        
+        
+       
     }
 
     public function delete_item(){
@@ -71,28 +182,141 @@ class CartController extends Controller
 
         $user_id = Auth::id();
 
-        $cart = User_cart::all()->where('id','=',$user_id);
+        if (Auth::check()) 
+        {
+            $cart = User_cart::all()->where('id','=',$user_id);
 
-        $cart = $cart->first();
+            $cart = $cart->first();
 
-        $items = Cart_items::all()->where('cart_id','=',$cart->id);
+            $items = Cart_items::all()->where('cart_id','=',$cart->id);
 
-        foreach ($items as $item){
-            $number = $item->product;
-            echo($number);
-            echo(" ");
-            echo($product_id);
-            echo(" ");
-            if ($number == $product_id){
-                echo("som tu ");
-                $id_to_delete = $item->id;
+            
+            foreach ($items as $item){
+                
+                $number = $item->product;
+                if ($number == $product_id)
+                {
+                    $id_to_delete = $item->id;
+                }
             }
+            
+            $del = Cart_items::find($id_to_delete);
+
+            $del->delete();
         }
 
-        $del = Cart_items::find($id_to_delete);
+        else
+        {
+            $value = Session::get('cart');
+            
+            if (Session::has('cart'))
+            {
+                Session::pull('cart');
+                $newlist = [];
+                foreach($value as $item)
+                {
+                    $product = $item[0];
+                    $counter = $item[1];
+                    if ($product == $product_id)
+                    {
+                        $item[0] = null;
+                        $item[1] = null;
+                    }
+                    else
+                    {
+                        
+                        
+                        $cartitem = [$item[0],$item[1]];
+                        
+                        Session::push('cart',$cartitem);
+                        Session::save();
+                       
+                    }
+                }
+            }
+            else
+            {
+                $list = [];
+            }
 
-        $del->delete();
+        }
+
+        
 
         return redirect('/kosik');
+    }
+
+
+    public function update_kosik(Request $request){
+        
+        $product_id = $request->product;
+        $counter =  $request->count;
+        $cart_id = $request->cart;
+
+        if (Auth::check())
+        {
+            $user_id = Auth::id();
+            $cart = User_cart::all()->where('id','=',$user_id);
+
+            $cart = $cart->first();
+
+            $items = Cart_items::all()->where('cart_id','=',$cart_id);
+
+            
+            foreach ($items as $item){
+                
+                $number = $item->product;
+                if ($number == $product_id)
+                {
+                    $id_to_update = $item->id;
+                }
+            }
+            
+            $updated = Cart_items::find($id_to_update);
+            $updated->counter = $counter;
+
+            $updated->save();
+            return response()->json(['success'=>$counter],);
+
+        }
+        else
+        {
+            $value = Session::get('cart');
+            
+            
+            Session::pull('cart');
+            $newlist = [];
+            
+            foreach($value as $item)
+            {
+                $product = $item[0];
+                
+                echo($product);
+                
+                if ($product == $product_id)
+                {
+                    $item[1] = $counter;
+                    $cartitem = [$item[0],$counter];
+                    Session::push('cart',$cartitem);
+                    Session::save();
+                    
+                }
+                else
+                {
+                    $cartitem = [$item[0],$item[1]];
+                    Session::push('cart',$cartitem);
+                    Session::save();
+                    
+                    
+                }
+                
+            }
+           
+            Session::save();
+            return response()->json(['success'=>$counter],);
+            
+           
+        }
+        
     }
 }
